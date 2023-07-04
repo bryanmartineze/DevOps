9456644ba3bf8dcd56a2a91017b77dd092f8ffd5
Bryan Carlos Martinez Elizalde <55309306+bryanmartineze@users.noreply.github.com>
2023-07-04 12:34:21 -0600

    .

--- terraform/aws/create-basic-vpc/main.tf
+++ terraform/aws/create-basic-vpc/main.tf
@@ -90,7 +90,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.example.id
 
     route {
-    gateway_id = aws_internet_gateway.internet_gateway.id
+    gateway_id = aws_internet_gateway.example.id
     cidr_block = "0.0.0.0/0"
   }
 
