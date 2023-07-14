0a33e7ac1583347a039dd920698d7511cb8380ab
Bryan Carlos Martinez Elizalde <55309306+bryanmartineze@users.noreply.github.com>
2023-07-14 12:53:29 -0600

    updated jenkins

--- jenkins/jenkins-installation-script.sh
+++ jenkins/jenkins-installation-script.sh
@@ -8,8 +8,14 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/
 sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum install jenkins -y
 
-#Access jenkins in the port 8080
+#Access jenkins in the port 8090
 sudo sed -i s/"JENKINS_PORT=8080"/"JENKINS_PORT=8090"/g /lib/systemd/system/jenkins.service
 
 #Initialize jenkins
 sudo systemctl enable --now jenkins
+
+#download the jenkins cli
+wget http://localhost:8090/jnlpJars/jenkins-cli.jar
+
+#initialize jenkins cli
+# sudo java -jar jenkins-cli.jar -s http://localhost:8090/ -auth <user>:<token>
