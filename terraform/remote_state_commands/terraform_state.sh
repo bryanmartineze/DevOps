fa554ca8af5d744938fab9f57ea4dc1fa931bdc2
Bryan Carlos Martinez Elizalde <55309306+bryanmartineze@users.noreply.github.com>
2023-07-24 18:06:33 -0600

    updated terraform

---
+++ terraform/remote_state_commands/terraform_state.sh
@@ -0,0 +1,15 @@
+#Check the resources created within a terraform configuration file
+
+terraform state list
+
+#show the state of a resource
+
+terraform state show <module>.<resource>
+
+#erase a resource from a configuration file
+
+terraform state rm local_file.hall_of_fame
+
+#rename a resource
+
+terraform state mv random_pet.super_pet_1 random_pet.ultra_pet
